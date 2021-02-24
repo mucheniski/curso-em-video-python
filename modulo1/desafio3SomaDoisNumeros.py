5d54b9a0034feaadd0c28b79394434657c66995c
mucheniski <mucheniski@gmail.com>
2021-02-24 19:00:56 -0300

    Aula 4 – Primeiros comandos em Python3

---
+++ modulo1/desafio3SomaDoisNumeros.py
@@ -0,0 +1,12 @@
+numero1 = input('Informe o primeiro numero ')
+numero2 = input('Informe o segundo numero ')
+
+def soma(n1, n2):
+    return n1 + n2
+
+resultado = soma(numero1, numero2)
+
+print('A soma é ' + resultado )
+
+
+
---
+++ modulo1/imprimirNomePesoIdade.py
@@ -0,0 +1,4 @@
+nome = input('Informe o nome ')
+idade = input('Informe a idade ')
+peso = input('Informe o peso ')
+print(nome, idade, peso)
