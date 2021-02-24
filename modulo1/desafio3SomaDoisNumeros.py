20b21c8c1b99f251776451fa6e8bf9e6a1b5d0ad
mucheniski <mucheniski@gmail.com>
2021-02-24 19:28:16 -0300

    Exercício 2 – Respondendo ao Usuário

--- modulo1/desafio3SomaDoisNumeros.py
+++ modulo1/desafio3SomaDoisNumeros.py
@@ -1,12 +1,12 @@
-numero1 = input('Informe o primeiro numero ')
-numero2 = input('Informe o segundo numero ')
+numero1 = int(input('Informe o primeiro numero '))
+numero2 = int(input('Informe o segundo numero '))
 
 def soma(n1, n2):
     return n1 + n2
 
 resultado = soma(numero1, numero2)
 
-print('A soma é ' + resultado )
+print('O resultado é ' , resultado )
 
 
 
