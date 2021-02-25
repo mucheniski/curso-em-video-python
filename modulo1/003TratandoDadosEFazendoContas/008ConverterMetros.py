42fe6c5ba921ec5cfe2e9c3d472f1aed6ec3d9f3
mucheniski <mucheniski@gmail.com>
2021-02-25 17:11:48 -0300

    Converter Metros em centimetros e milimetros

---
+++ modulo1/003TratandoDadosEFazendoContas/008ConverterMetros.py
@@ -0,0 +1,4 @@
+medidaEmMetros = int(input('Informe o valor em metros: '))
+medidaEmCentimetros = medidaEmMetros * 100
+medidaEmMilimetros = medidaEmMetros * 1000
+print('O valor em centímetros é {} e o valor em milímetros é {}'.format(medidaEmCentimetros, medidaEmMilimetros))
