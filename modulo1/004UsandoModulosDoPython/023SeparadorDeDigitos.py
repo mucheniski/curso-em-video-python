c66e28b02e1e2961047201c7de304582667a6cf0
mucheniski <mucheniski@gmail.com>
2021-03-04 19:12:11 -0300

    023 separando digitos do número unidade, dezena, centena e milhar

---
+++ modulo1/004UsandoModulosDoPython/023SeparadorDeDigitos.py
@@ -0,0 +1,11 @@
+# Exercício Python 23: Faça um programa que leia um número de 0 a 9999
+# e mostre na tela cada um dos dígitos separados.
+numero = int(input('Informe um número: '))
+unidade = numero // 1 % 10
+dezena = numero // 10 % 10
+centena = numero // 100 % 10
+milhar = numero // 1000 % 10
+print('Unidade {}: '.format(unidade))
+print('Dezena {}: '.format(dezena))
+print('Centena {}: '.format(centena))
+print('Milhar {}: '.format(milhar))
