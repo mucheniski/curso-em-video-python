364d90c1e6ddb8ea87b791410fe676d88a0c5583
mucheniski <mucheniski@gmail.com>
2021-03-08 18:12:43 -0300

    030 verificar numero par ou impar

---
+++ modulo1/005CondicoesPython/030ParOuImpar.py
@@ -0,0 +1,6 @@
+# Exercício Python 30: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+numero = int(input('Informe o número: '))
+if numero % 2 == 0:
+    print('Número par')
+else:
+    print('Número impar')
