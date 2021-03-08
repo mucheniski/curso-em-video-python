ea3f5368363d37451cbe7a494692a8a0fd720b59
mucheniski <mucheniski@gmail.com>
2021-03-08 18:18:12 -0300

    031 calculando o valor da viagem por KM

---
+++ modulo1/003TratandoDadosEFazendoContas/031CalcularValorViagemPorKM.py
@@ -0,0 +1,11 @@
+# Exercício Python 31: Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem,
+# cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
+distanciaKM = float(input('Informe a distancia em KM: '))
+valorViagem = 0.0
+
+if distanciaKM <= 200.0:
+    valorViagem = distanciaKM * 0.5
+else:
+    valorViagem = distanciaKM * 0.45
+
+print('O Valor da viagem é de R$ {}'.format(valorViagem))
