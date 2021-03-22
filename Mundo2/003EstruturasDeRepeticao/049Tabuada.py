80f6a0b85a5b3bb7b694289d02ee8bd6c8a70918
mucheniski <mucheniski@gmail.com>
2021-03-22 20:07:09 -0300

    049 Tabuada

---
+++ Mundo2/003EstruturasDeRepeticao/049Tabuada.py
@@ -0,0 +1,8 @@
+# Exercício Python 49: Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher,
+# só que agora utilizando um laço for.
+
+soma = 0
+numero = int(input('Informe o número para a tabuada: '))
+for i in range(1, 11):
+    soma = i * numero
+    print('{} X {} = {}'.format(numero, i, soma))
