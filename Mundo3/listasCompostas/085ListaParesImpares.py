0c0386150498c56bfa43dd91daf18bb12aa4d61d
mucheniski <mucheniski@gmail.com>
2021-04-29 14:43:29 -0300

    085 lista pares e impares

---
+++ Mundo3/listasCompostas/085ListaParesImpares.py
@@ -0,0 +1,21 @@
+# Exercício Python 085: Crie um programa onde o usuário possa digitar sete valores
+# numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e
+# ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+listaValores = list()
+listaPares = list()
+listaImpares = list()
+
+for i in range(0, 7):
+    numero = int(input('Informe um número: '))
+    if numero % 2 == 0:
+        listaPares.append(numero)
+    else:
+        listaImpares.append(numero)
+
+listaPares.sort()
+listaImpares.sort()
+listaValores.append(listaPares[:])
+listaValores.append(listaImpares[:])
+print(listaValores)
+
+
