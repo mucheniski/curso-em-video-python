6de6f48011d4c7cb5839b1d278999fe455ef500f
mucheniski <mucheniski@gmail.com>
2021-04-29 18:48:55 -0300

    086 Matriz

---
+++ Mundo3/listasCompostas/086Matriz.py
@@ -0,0 +1,10 @@
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+for linha in range(0,3):
+    for coluna in range(0,3):
+        matriz[linha][coluna] = int(input(f'Informe o valor para [{linha}][{coluna}]: '))
+
+print('='*50)
+for linha in range(0,3):
+    for coluna in range(0,3):
+        print(f'[{matriz[linha][coluna]:^5}]', end='')
+    print()
