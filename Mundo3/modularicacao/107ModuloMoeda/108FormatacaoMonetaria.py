f7eab743c676a215adbe76dc0ea94b0862d3dc6e
mucheniski <mucheniski@gmail.com>
2021-05-19 15:30:58 -0300

    108 formatacao de moeda

---
+++ Mundo3/modularicacao/107ModuloMoeda/108FormatacaoMonetaria.py
@@ -0,0 +1,10 @@
+# Exercício Python 108: Adapte o código do desafio #107, criando uma função adicional chamada moeda()
+# que consiga mostrar os números como um valor monetário formatado.
+
+import moeda
+
+print(moeda.aumentar(1))
+print(moeda.diminuir(5))
+print(moeda.dobro(2))
+print(moeda.metade(10))
+print(moeda.formataMoeda(10.00))
