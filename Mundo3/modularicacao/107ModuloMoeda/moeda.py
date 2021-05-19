9def3095844b2a4cbc50cea5da037995601899bc
mucheniski <mucheniski@gmail.com>
2021-05-19 15:24:55 -0300

    107 modulo moeda

---
+++ Mundo3/modularicacao/107ModuloMoeda/moeda.py
@@ -0,0 +1,18 @@
+# Exercício Python 107: Crie um módulo chamado moeda.py que tenha as funções incorporadas
+# aumentar(), diminuir(), dobro() e metade().
+# Faça também um programa que importe esse módulo e use algumas dessas funções.
+
+def aumentar(valor):
+    return valor + 1
+
+
+def diminuir(valor):
+    return valor - 1
+
+
+def dobro(valor):
+    return valor * 2
+
+
+def metade(valor):
+    return valor / 2
---
+++ Mundo3/modularicacao/107ModuloMoeda/programaPrincipalMoeda.py
@@ -0,0 +1,6 @@
+import moeda
+
+print(moeda.aumentar(1))
+print(moeda.diminuir(5))
+print(moeda.dobro(2))
+print(moeda.metade(10))
